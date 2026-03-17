6e4fde92b8f86bbbe459c722dd502acea321bba8
Benjamin <benjacruzvejar@gmail.com>
2026-03-17 09:42:59 -0600

    Fix final: Tailwind v4 configurado y cache limpia

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // Con el @ y con el /postcss
+    "autoprefixer": {},
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // Deja solo React por ahora
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: 'dist',
   },
   base: './',
 });
